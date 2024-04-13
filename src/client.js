80ed09168da01534fe702ecf7471fe1b68bef033
appdep <appdeployment64@gmail.com>
2024-04-13 05:43:57 -0700

    first commit

--- src/client.js
+++ src/client.js
@@ -2,7 +2,7 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-  projectId: REACT_APP_SANITY_PROJECT_ID,
+  projectId:REACT_APP_SANITY_PROJECT_ID,
   dataset: 'production',
   apiVersion: '2024-02-01',
   useCdn: true,
